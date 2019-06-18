6bed277789953cb57c2cbf166febff609fb71779
Luis Lugo <l.lugo@premiosgroup.com>
2019-06-18 07:27:47 -0400

    Add project files.

---
+++ Mod1_Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating a Car object using type inference
+            var Car1 = new Car();
+            Car1.Color = "White";
+            Car1.Year = 2010;
+            Car1.Mileage = 11000;
+
+            // Output to the console window
+            Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles on it.");
+
+        }
+    }
+
+    // Declaring the Car() class
+    public class Car
+    {
+        // Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+    }
+
+}
