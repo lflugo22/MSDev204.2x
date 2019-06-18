44d199790f2d296bf21301216d0e175ce9450a70
Luis Lugo <l.lugo@premiosgroup.com>
2019-06-18 08:58:48 -0400

    Lab 3 - Organizing the classes

---
+++ Mod1_Lab2/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod1_Lab2
+{
+    // Declaring the Car() class
+    public class Car
+    {
+        // Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        // Adding a constructor
+        // This constructor instantiates a new Car when only the color and year are available.
+        public Car(string color, int year)
+        {
+            this.Color = color;
+            this.Year = year;
+
+            // Increment the number of car instances
+            instances++;
+        }
+
+        // Adding another constructor
+        // This constructor instantiates a new Car using the year and mileage.
+        public Car(int year, int mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+
+            // Increment the number of car instances
+            instances++;
+        }
+
+        // Creates integer variable called "instances" and assigns it a value of 0.
+        private static int instances = 0;
+
+        public Car()
+        {
+            // Increment instances every time the constructor runs
+            instances++;
+        }
+
+        // Declare static member that returns the instance count
+        public static int CountCars()
+        {
+            return instances;
+        }
+
+    }
+
+}
--- Mod1_Lab2/Program.cs
+++ Mod1_Lab2/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mod1_Lab1
+namespace Mod1_Lab2
+
 {
     class Program
     {
@@ -25,53 +26,6 @@ namespace Mod1_Lab1
             // Output to the console window
             Console.WriteLine($"There are {carCount} cars in inventory right now.");
         }
-    }
-
-    // Declaring the Car() class
-    public class Car
-    {
-        // Defining properties
-        public string Color { get; set; }
-        public int Year { get; set; }
-        public int Mileage { get; set; }
-
-        // Adding a constructor
-        // This constructor instantiates a new Car when only the color and year are available.
-        public Car (string color, int year)
-        {
-            this.Color = color;
-            this.Year = year;
-
-            // Increment the number of car instances
-            instances++;
-        }
-
-        // Adding another constructor
-        // This constructor instantiates a new Car using the year and mileage.
-        public Car (int year, int mileage)
-        {
-            this.Year = year;
-            this.Mileage = mileage;
-
-            // Increment the number of car instances
-            instances++;
-        }
-
-        // Creates integer variable called "instances" and assigns it a value of 0.
-        private static int instances = 0;
-        
-        public Car()
-        {
-            // Increment instances every time the constructor runs
-            instances++;
-        }
-
-        // Declare static member that returns the instance count
-        public static int CountCars()
-        {
-            return instances;
-        }
-            
     }
 
 }
