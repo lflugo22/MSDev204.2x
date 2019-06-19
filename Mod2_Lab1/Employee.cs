d38b7ab68b737a0734cf845298c46c9fe67fc2ff
Luis Lugo <l.lugo@premiosgroup.com>
2019-06-19 10:10:08 -0400

    Module 2 Lab 1

---
+++ Mod2_Lab1/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
+{
+    public class Employee
+    {
+        private static int employeeCount = 1;
+
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+
+        public string Name { get => employeeName; set => employeeName = value; }
+        public double BaseSalary { get => employeeBaseSalary; set => employeeBaseSalary = value; }
+        public int ID { get => employeeId; set => employeeId = value; }
+
+        public Employee(string name, double baseSalary)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+            ID = employeeCount++;
+        }
+
+        public string toString() => ID + " " + this.Name;
+
+        public virtual string employeeStatus() => toString() + " is in the company's system.";
+     
+
+}
+}
